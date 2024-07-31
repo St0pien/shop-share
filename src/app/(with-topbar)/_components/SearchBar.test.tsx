51ae5e1c2cfd25d394b8babb08f223a58b43164e
Jakub Stępień <jstepien50@gmail.com>
2024-07-31 15:13:22 +0200

    Move components into correct location

---
+++ src/app/(with-topbar)/_components/SearchBar.test.tsx
@@ -0,0 +1,11 @@
+import { describe, test } from 'vitest';
+import { render } from '@testing-library/react';
+
+
+describe('SearchBar', () => {
+  test('should reflect input into URL', () => {
+    render(<h1>hello there</h1>);
+
+    await u
+  });
+});
