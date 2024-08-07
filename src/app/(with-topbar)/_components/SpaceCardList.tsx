61254c4002cb1fc79f1a7dd80236bcc01746df52
Jakub Stępień <jstepien50@gmail.com>
2024-08-07 17:12:43 +0200

    Hook spaces searching to frontend

--- src/app/(with-topbar)/_components/SpaceCardList.tsx
+++ src/app/(with-topbar)/_components/SpaceCardList.tsx
@@ -1,14 +1,18 @@
 'use client';
 
 import { api } from '@/trpc/react';
+import { Spinner } from '@/components/svg/Spinner';
 
 import { SpaceCard } from './SpaceCard';
 
-export function SpaceCardList() {
-  const [spaces ] = api.spaces.fetch.useSuspenseQuery();
+export function SpaceCardList({ search }: { search?: string }) {
+  const [spaces] = api.spaces.fetch.useSuspenseQuery({
+    search
+  });
 
   return (
     <>
+      {spaces.length === 0 && <p className='text-neutral-light text-xl'>No spaces found</p>}
       {spaces.map(space => (
         <SpaceCard key={space.id} spaceInfo={space} />
       ))}
--- src/app/(with-topbar)/page.tsx
+++ src/app/(with-topbar)/page.tsx
@@ -8,20 +8,24 @@ import { Spinner } from '@/components/svg/Spinner';
 import { AddSpaceDialog } from './_components/AddSpaceDialog';
 import { SpaceCardList } from './_components/SpaceCardList';
 
-export default async function HomePage() {
+export default async function HomePage({
+  searchParams
+}: {
+  searchParams: { search?: string };
+}) {
   const session = await auth();
 
   if (!session) {
     redirect('/login');
   }
 
-  void api.spaces.fetch.prefetch();
+  void api.spaces.fetch.prefetch({ search: searchParams.search });
 
   return (
     <HydrateClient>
       <div className='flex w-full flex-col items-center gap-8'>
-        <Suspense fallback={<Spinner className='w-20 h-20' />}>
-          <SpaceCardList />
+        <Suspense key={searchParams.search} fallback={<Spinner className='h-20 w-20' />}>
+          <SpaceCardList search={searchParams.search} />
         </Suspense>
       </div>
       <div className='fixed bottom-32 right-8'>
