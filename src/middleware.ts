15bbc9bf73e1ac8c42c7f3903b259c9ad294ba5c
Jakub Stępień <jstepien50@gmail.com>
2024-08-19 22:58:18 +0200

    Add production session cookie pattern to middleware

---
+++ src/middleware.ts
@@ -0,0 +1,31 @@
+import { type NextRequest, NextResponse } from 'next/server';
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico|login).*)'
+  ]
+};
+
+export async function middleware(req: NextRequest) {
+  if (req.cookies.has('authjs.session-token')) {
+    return NextResponse.next();
+  }
+
+  if (req.cookies.has('__Secure-authjs.session-token')) {
+    return NextResponse.next();
+  }
+
+  const signInUrl = new URL(
+    `/login?callback=${encodeURIComponent(req.url)}`,
+    new URL(req.url).origin
+  );
+
+  return NextResponse.redirect(signInUrl);
+}
