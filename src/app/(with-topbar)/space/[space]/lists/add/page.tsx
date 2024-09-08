adb11fd89836f2bf4fa7361d189fbb7c7b11bb71
Jakub Stępień <jstepien50@gmail.com>
2024-09-08 16:33:18 +0200

    Fix incorrect prefetching

--- src/app/(with-topbar)/space/[space]/lists/add/page.tsx
+++ src/app/(with-topbar)/space/[space]/lists/add/page.tsx
@@ -5,7 +5,7 @@ import { api, HydrateClient } from '@/trpc/server';
 export default function AddListPage({ params }: { params: { space: string } }) {
   const returnUrl = `/space/${params.space}`;
 
-  void api.category.fetch.prefetch(uuidTranslator.toUUID(params.space));
+  void api.list.fetch.prefetch(uuidTranslator.toUUID(params.space));
 
   return (
     <HydrateClient>
